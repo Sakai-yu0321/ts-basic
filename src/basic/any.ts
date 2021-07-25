cdc1b5f7f60583df7cf17609abbe441d2adfd4c0
Sakai-yu0321 <on99.yu.sakai@gmail.com>
2021-07-26 08:01:13 +0900

    any型

--- src/basic/any.ts
+++ src/basic/any.ts
@@ -5,7 +5,3 @@ export default function anySample() {
   name = 28
   console.log("any sample2:", typeof name, name);
 }
-
-//unknown型を使う場面→帰ってくる値がどんな型なのががわからない時。
-//APIを投げて帰ってくるのが、string型なのかnumber型なのか、object型なのかそれとも配列型なのか...
-//一旦unknown型にしておいて、型によって処理を振り分けるようにして使う。
